788f695a561adf7b607b47b66fba2db753ae1b5d
Marcin Cieśla <Marcin Ciela>
2016-05-15 23:57:11 +0200

    init

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+eventsApp.controller('EventController',
+  function EventController($scope) {
+    $scope.event = {
+      name: 'Angular Boot Camp',
+      date: '2016-01-01',
+      time: '10:30',
+      location: {
+        address: 'Postępu 14',
+        city: 'Warsaw',
+        country: 'Poland'
+      }
+    }
+  }
+);
